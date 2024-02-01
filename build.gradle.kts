c283744ca394f6bb516a7b75880d3d398743c675
jihwan2da <wlghks8982@naver.com>
2024-02-01 23:58:06 +0900

    feat: 프로젝트 초기 설정

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.jpa") version "1.9.22"
+}
+
+group = "com.gloddy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.amazonaws:aws-java-sdk-dynamodb:1.12.497")
+	implementation("io.github.boostchicken:spring-data-dynamodb:5.2.5")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.batch:spring-batch-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/gloddy/batch/config/dynamodb/DynamoDBConfig.kt
@@ -0,0 +1,68 @@
+package com.gloddy.batch.config.dynamodb
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.datamodeling.ConversionSchemas
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverterFactory
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = ["com.gloddy.batch.model.dynamodb"], dynamoDBMapperConfigRef = "dynamoDBMapperConfig")
+class DynamoDBConfig(
+    @Value("\${amazon.dynamodb.endpoint}") private val endpoint: String,
+    @Value("\${amazon.aws.accessKey}") private val accessKey: String,
+    @Value("\${amazon.aws.secretKey}") private val secretKey: String,
+    @Value("\${amazon.aws.region}") private val region: String,
+    @Value("\${stage}") private val stage: String
+) {
+
+    @Primary
+    @Bean
+    fun dynamoDBMapper(amazonDynamoDB: AmazonDynamoDB): DynamoDBMapper {
+        return DynamoDBMapper(
+            amazonDynamoDB,
+            dynamoDBMapperConfig()
+        )
+    }
+
+    @Bean
+    fun dynamoDBMapperConfig(): DynamoDBMapperConfig {
+        return DynamoDBMapperConfig.builder()
+            .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE)
+            .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.EVENTUAL)
+            .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.LAZY_LOADING)
+            .withTableNameResolver(tableNameResolver())
+            .withBatchWriteRetryStrategy(DynamoDBMapperConfig.DefaultBatchWriteRetryStrategy.INSTANCE)
+            .withBatchLoadRetryStrategy(DynamoDBMapperConfig.DefaultBatchLoadRetryStrategy.INSTANCE)
+            .withTypeConverterFactory(DynamoDBTypeConverterFactory.standard())
+            .withConversionSchema(ConversionSchemas.V2_COMPATIBLE)
+            .build()
+    }
+
+    @Bean
+    fun amazonDynamoDB(): AmazonDynamoDB {
+        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
+        val awsCredentialsProvider = AWSStaticCredentialsProvider(awsCredentials)
+        val endpointConfiguration = AwsClientBuilder.EndpointConfiguration(endpoint, region)
+        return AmazonDynamoDBClientBuilder.standard()
+            .withCredentials(awsCredentialsProvider)
+            .withEndpointConfiguration(endpointConfiguration)
+            .build()
+    }
+
+    @Bean
+    fun awsCredentials() = BasicAWSCredentials(accessKey, secretKey)
+
+    @Bean
+    @Primary
+    fun tableNameResolver() = TableNameResolver(stage)
+}
